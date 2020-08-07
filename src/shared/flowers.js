d9f2b312fbdd5e324d3a1e9b57aff72ea57fed53
betsystevens <betsystevens5@gmail.com>
2020-08-06 21:12:33 -0500

    add flowers data file

---
+++ src/shared/flowers.js
@@ -0,0 +1,35 @@
+export const FLOWERS = 
+  [
+    {
+      id: 0,
+      name: 'Hanging Baskets',
+      image: 'assets/images/hb/petuniaPinkPurpleHB750x500.jpg',
+      container: 'hb',
+      price: '24.00',
+      description: 'Our 10-inch flower baskets of unbeatable quality, regularly sell out. Never disappointing, these spectacular baskets keep customers coming back year after year. The wave petunias and ivy geraniums are unrivaled in their beauty and quality.',
+    },
+    {
+      id: 1,
+      name: 'Herbs and Tomatoes',
+      image: 'assets/images/pots/basilHerb720x480.jpg',
+      container: '4 inch pot',
+      price: '3.00 and 4.00',
+      description: 'Choose from our selection of popular herbs: basil, oregano, parsley, rosemary, chives, and cilantro. For the crown jewel of the home vegetable garden, delicious home-grown tomatoes, we offer several varieties in 4-inch pots.'
+    },
+    {
+      id: 2,
+      name: 'Alyssum Mixed',
+      image: 'assets/images/flats/alyssumMixed750x563.jpg',
+      container: 'flat',
+      price: '17.00',
+      description: 'Carefree, low, spreading plants grown for their flowers and are useful in containers or for edging flower beds. Sweet Alyssum thrives in full sun or partial shade and prefers consistently moist, well-drained soil. Water regularly, especially if grown in hanging baskets or containers.'
+    },
+    {
+      id: 3,
+      name: 'Accent Plants',
+      image: 'assets/images/pots/potatoVine750x499.jpg',
+      container: '4 inch pot',
+      price: '4.00',
+      description: 'Versatile plants to grow in containers and flower beds. These are loved for their colorful foliage and leaf shapes. They grow fast and fill space quickly, adding interest wherever they are placed.'
+    }
+  ];  
