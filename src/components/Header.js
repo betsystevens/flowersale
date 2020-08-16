3067b6b02797d9a264b109153754b093e05094a3
betsystevens <betsystevens5@gmail.com>
2020-08-15 22:57:41 -0500

    add nav links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 function Header(props) {
 
   return (
-    <Navbar dark color="primary">
+    <Navbar dark expand="md" color="primary">
       <div className="container">
         <NavbarBrand href="/">Flowers For Sale</NavbarBrand>
+        <Nav navbar navbar-default className="mr-auto">
+          <NavItem>
+            <NavLink className="nav-link px-4" href='/flats'>Flats</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link px-4" href='/hb'>Hanging Baskets</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link px-4" href='/pots'>Potted Plants</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link px-4" href='/tomatoes'>Tomatoes & Herbs</NavLink>
+          </NavItem>
+        </Nav>    
       </div>
     </Navbar>
   );
