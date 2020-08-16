f6ba8df3d588a3ff9e6d3b8b1397c70d9be85076
betsystevens <betsystevens5@gmail.com>
2020-08-15 20:26:03 -0500

    App.js imports flats.js data file, use as state and propss

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import FlowerGroups from './components/FlowerGroupsComponent';
-import './App.css';
+import FlowerGroups from './components/FlowerGroups';
 import { FLOWERS } from './shared/flowers';
+import { FLATS } from './shared/flats';
+import FlowerCards from './components/FlowerCards';
 import FlowerCarousel from './components/FlowerCarousel';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      flowers: FLOWERS
+      flowers: FLOWERS,
+      flats: FLATS
     };
   }
   render() {
@@ -22,6 +25,7 @@ class App extends Component {
         </Navbar>
         <FlowerCarousel />
         <FlowerGroups flowers={this.state.flowers}/>
+        <FlowerCards flats={this.state.flats}/>
       </div>
     );
   }
---
+++ src/shared/flats.js
@@ -0,0 +1,157 @@
+export const FLATS =
+[
+  {
+    id: 1,
+    name: 'Alyssum Mixed',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/alyssumMixed512x138.jpg'
+  },
+  {
+    id: 2,
+    name: 'Begonia Pink Bronze Leaf',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/begoniaBronzeLeafPink512x384.jpg'
+  },
+  {
+    id: 3,
+    name: 'Begonia Red',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/begoniaRed512x384.jpg'
+  },
+  {
+    id: 4,
+    name: 'Begonia Red Bronze Leaf',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/begoniaRedBronzeLeaf512x384.jpg'
+  },
+  {
+    id: 5,
+    name: 'Coleus Scarlet',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/coleusScarlet512x384.jpg'
+  },
+  {
+    id: 6,
+    name: 'Coleus Wizard',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/coleusWizardMix512x384.jpg'
+  },
+  {
+    id: 7,
+    name: 'Dahlia Mixed',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/dahliaMixed512x384.jpg'
+  },
+  {
+    id: 8,
+    name: 'Dianthus Red',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/dianthusRed512x384.jpg'
+  },
+  {
+    id: 9,
+    name: 'Dusty Miller',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/dustyMiller512x384.jpg'
+  },
+  {
+    id: 10,
+    name: 'Impatiens Mixed',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/impatiensPastelMix512x384.jpg'
+  },
+  {
+    id: 11,
+    name: 'Impatiens Red',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/impatiensRed512x384.jpg'
+  },
+  {
+    id: 12,
+    name: 'Impatiens White',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/impatiensWhite512x384.jpg'
+  },
+  {
+    id: 13,
+    name: 'Marigold Mixed',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/marigoldMixed512x384.jpg'
+  },
+  {
+    id: 14,
+    name: 'Marigold Yellow',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/marigoldYellow512x384.jpg'
+  },
+  {
+    id: 15,
+    name: 'Petunia Mixed',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/petuniaMixed512x384.jpg'
+  },
+  {
+    id: 16,
+    name: 'Petunia White',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/petuniaWhite512x384.jpg'
+  },
+  {
+    id: 17,
+    name: 'Salvia Red',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/salviaRed512x384.jpg'
+  },
+  {
+    id: 18,
+    name: 'Salvia Victoria Blue',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/salviaVictoriaBlue512x384.jpg'
+  },
+  {
+    id: 19,
+    name: 'Snap Dragons Dwarf',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/snapDragonMix512x384.jpg'
+  },
+  {
+    id: 20,
+    name: 'Snap Dragons Tall',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/snapDragonsTall512x384.jpg'
+  },
+  {
+    id: 21,
+    name: 'Vinca Mixed',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/vincaMixed512x384.jpg'
+  },
+  {
+    id: 22,
+    name: 'Zinnia Mixed',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/assets/images/flats/zinniaMixed512x384.jpg'
+  }
+];
