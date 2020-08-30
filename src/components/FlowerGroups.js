10547ba7f6c137b37787ea93c41634e0a5e1ca6f
betsystevens <betsystevens5@gmail.com>
2020-08-30 12:54:16 -0500

    change class component to function

--- src/components/FlowerGroups.js
+++ src/components/FlowerGroups.js
@@ -1,37 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Media } from 'reactstrap';
 
-class FlowerGroups extends Component {
-  render() {
-    const group = this.props.flowers.map((flower) => {
-      const imgStyle = {
-        width: 225,
-        height: 150 
-      }
-      return (
-        <div key={flower.id} className="col-12 mt-5">
-          <Media tag="li">
-            <Media left middle>
-              <Media object className="shadow p-2 bg-white rounded" src={flower.image} alt={flower.name} style={imgStyle} />
-            </Media>
-            <Media body className="ml-5">
-              <Media heading>{flower.name}</Media>
-              <p>{flower.description}</p>
-            </Media>
-          </Media>
-        </div>
-      );
-    });
+// component lays out top level groups of flowers, hb, flats, pots, herbs
+// reactStrap Media displays image, heading and paragraph for each group
+function FlowerGroups(props) {
+  const group = props.flowers.map((flower) => {
+    const imgStyle = {
+      width: 225,
+      height: 150 
+    }
     return (
-      <div className="container">
-        <div className="row">
-          <Media list>
-            {group}
+      <div key={flower.id} className="col-12 mt-5">
+        <Media tag="li">
+          <Media left middle>
+            <Media object className="shadow p-2 bg-white rounded" src={flower.image} alt={flower.name} style={imgStyle} />
           </Media>
-        </div>
+          <Media body className="ml-5">
+            <Media heading>{flower.name}</Media>
+            <p>{flower.description}</p>
+          </Media>
+        </Media>
       </div>
     );
-  };
-}
+  });
+  return (
+    <div className="container">
+      <div className="row">
+        <Media list>
+          {group}
+        </Media>
+      </div>
+    </div>
+  );
+};
 
 export default FlowerGroups;
