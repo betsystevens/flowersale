0089ad4132e9b94ee41130b3e9ca89e995aa5dfe
betsystevens <betsystevens5@gmail.com>
2020-08-30 19:33:32 -0500

    add container component

---
+++ src/components/Main.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Header from  './Header';
+import FlowerGroups from './FlowerGroups';
+import FlowerCards from './FlowerCards';
+import { FLOWERS } from '../shared/flowers';
+import { FLATS } from '../shared/flats';
+
+import '../App.css';
+
+function Main(props) {
+
+  const data = {
+    flowers : FLOWERS,
+    flats : FLATS
+  }
+
+  const HomePage = () => {
+    return(
+      <FlowerGroups flowers={data.flowers} />
+    )
+  }
+  return (
+
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/home" component={HomePage} />
+        <Route exact path="/flats" component={() => <FlowerCards flats={data.flats} />} />
+        <Redirect to="/home" />
+      </Switch>
+    </div>
+  )
+}
+
+export default Main;
