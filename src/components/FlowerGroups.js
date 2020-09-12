5ba94c092413dd96dfb3049a3427e73486f38ecb
betsystevens <betsystevens5@gmail.com>
2020-09-11 21:32:58 -0500

    replace bootstrap & reactstrap media with css and flex

--- src/components/FlowerGroups.js
+++ src/components/FlowerGroups.js
@@ -1,35 +1,21 @@
 import React from 'react';
-import { Media } from 'reactstrap';
 
 // component lays out top level groups of flowers, hb, flats, pots, herbs
-// reactStrap Media displays image, heading and paragraph for each group
 function FlowerGroups(props) {
   const group = props.flowers.map((flower) => {
-    const imgStyle = {
-      width: 225,
-      height: 150 
-    }
     return (
-      <div key={flower.id} className="col-12 mt-5">
-        <Media tag="li">
-          <Media left middle>
-            <Media object className="shadow p-2 bg-white rounded" src={flower.image} alt={flower.name} style={imgStyle} />
-          </Media>
-          <Media body className="ml-5">
-            <Media heading>{flower.name}</Media>
+      <div key={flower.id} className="band shadow">
+          <img src={flower.image} alt={flower.name} />
+          <div className="band-body">
+            <h3>{flower.name}</h3>
             <p>{flower.description}</p>
-          </Media>
-        </Media>
+          </div>
       </div>
     );
   });
   return (
-    <div className="container">
-      <div className="row">
-        <Media list>
-          {group}
-        </Media>
-      </div>
+    <div className="outer">
+      {group}
     </div>
   );
 };
