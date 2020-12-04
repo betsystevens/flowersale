b7b26dd02f077cadd82f5338058be4ac15caa1f2
betsystevens <betsystevens5@gmail.com>
2020-12-03 22:42:28 -0600

    add tailwind styles to cards

--- src/components/FlowerCards.js
+++ src/components/FlowerCards.js
@@ -1,19 +1,24 @@
 import React from 'react';
 
-function FlowerCards(props) {
+function FCard2(props) {
   const flatInfo = props.flowers.filter((flower) => flower.container === 'flat')
   const price = flatInfo[0].price
   const flatCards = props.flats.map((flat) => {
     return (
-      <div key={flat.id}>
-        <div className="card">
-          <div className="card-header">
-            <img src={flat.image} alt={flat.name} />
+      <div key={flat.id} >
+        <div className="m-5 w-60 shadow-lg">
+          <div className="">
+            <img src={flat.image} alt={flat.name} 
+                  className="p-4"
+            />
           </div>
-          <div className="card-body">
-            <h2 className="card-title">{flat.name}</h2>
-            <p className="card-text">{`36 plants per flat - ${price}`}</p>
-            <p className="card-text">{flat.description}</p>
+          <div className="p-2 flex flex-col items-center">
+            <h2 className="pt-2 font-extrabold">{flat.name}</h2>
+            <p className="pt-2 pb-4">{`Price - ${price}`}</p>
+            <button className="mb-4 px-8 py-2 rounded-lg 
+                              bg-pink-400 ">
+            Add to Bag
+            </button>
           </div>
         </div>
       </div>
