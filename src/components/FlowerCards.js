88b39cd74bee3312eb14e76243cc49ee30472738
betsystevens <betsystevens5@gmail.com>
2020-12-04 21:13:09 -0600

    add styles to card & elements, retrieve new property from data file

--- src/components/FlowerCards.js
+++ src/components/FlowerCards.js
@@ -1,23 +1,27 @@
 import React from 'react';
 
 function FlowerCards(props) {
-  const flatInfo = props.flowers.filter((flower) => flower.container === 'flat')
+  const flatInfo = props.flowers.filter((flower) => flower.container.name === 'flat')
   const price = flatInfo[0].price
+  const containerInfo = flatInfo[0].container.description
   const flatCards = props.flats.map((flat) => {
     return (
       <div key={flat.id} >
-        <div className="m-5 w-60 shadow-lg">
+        <div className="m-5 w-60 shadow-lg border border-white hover:border-purple-200">
           <div className="">
             <img src={flat.image} alt={flat.name} 
-                  className="p-4"
-            />
+                 className="px-4 pt-4 pb-6" />
           </div>
           <div className="p-2 flex flex-col items-center">
-            <h2 className="pt-2 font-extrabold">{flat.name}</h2>
-            <p className="pt-2 pb-4">{`Price - ${price}`}</p>
-            <button className="mb-4 px-8 py-2 rounded-lg 
-                              bg-pink-400 ">
-            Add to Bag
+            <p className="font-extrabold text-lg">{flat.name}</p>
+            <p className="pt-1">{`Price - ${price}`}</p>
+            <p className="pt-1">{`${containerInfo}`}</p>
+          </div>
+          <div className="flex flex-col items-center w-full p-4 pb-7">
+            <button 
+              className="px-8 pb-2 hover:bg-purple-100
+                         fancy-button">
+              Add to Cart
             </button>
           </div>
         </div>
