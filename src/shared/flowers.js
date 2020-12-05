06648a8d8e76136fe4b9786e4bff2655bd06ebcc
betsystevens <betsystevens5@gmail.com>
2020-12-04 20:40:55 -0600

    add short container description to data file

--- src/shared/flowers.js
+++ src/shared/flowers.js
@@ -4,15 +4,21 @@ export const FLOWERS =
       id: 0,
       name: 'Hanging Baskets',
       image: 'assets/images/hb/petuniaPinkPurpleHB750x500.jpg',
-      container: 'hb',
+      container: {
+        name: 'hb',
+        description: '10-inch green basket' 
+      },
       price: '24.00',
-      description: 'Our 10-inch flower baskets of unbeatable quality, regularly sell out. Never disappointing, these spectacular baskets keep customers coming back year after year. The wave petunias and ivy geraniums are unrivaled in their beauty and quality.',
+      description: 'Our 10-inch flower baskets are of unbeatable quality. They regularly sell out every year. Never disappointing, these spectacular baskets keep customers coming back year after year. The wave petunias and ivy geraniums are unrivaled in their beauty and quality.',
     },
     {
       id: 1,
       name: 'Flats',
       image: 'assets/images/flats/alyssumMixed750x563.jpg',
-      container: 'flat',
+      container: {
+        name: 'flat',
+        description: '36 plants per flat'
+      },
       price: '17.00',
       description: 'Carefree, low, spreading plants grown for their flowers and are useful in containers or for edging flower beds. Sweet Alyssum thrives in full sun or partial shade and prefers consistently moist, well-drained soil. Water regularly, especially if grown in hanging baskets or containers.'
     },
@@ -20,7 +26,10 @@ export const FLOWERS =
       id: 2,
       name: 'Potted Plants',
       image: 'assets/images/pots/potatoVine750x499.jpg',
-      container: '4 inch pot',
+      container: {
+        name: '4 inch pot',
+        description: '4 inch pot'
+      },
       price: '4.00',
       description: 'Versatile plants to grow in containers and flower beds. These are loved for their colorful foliage and leaf shapes. They grow fast and fill space quickly, adding interest wherever they are placed.'
     },
@@ -28,8 +37,11 @@ export const FLOWERS =
       id: 3,
       name: 'Herbs and Tomatoes',
       image: 'assets/images/pots/basilHerb720x480.jpg',
-      container: '4 inch pot',
+      container: {
+        name: '4 inch pot',
+        description: '4 inch pot'
+      },
       price: '3.00 and 4.00',
       description: 'Choose from our selection of popular herbs: basil, oregano, parsley, rosemary, chives, and cilantro. For the crown jewel of the home vegetable garden, delicious home-grown tomatoes, we offer several varieties in 4-inch pots.'
-    }
+    },
   ];  
