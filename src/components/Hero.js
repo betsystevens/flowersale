dd8795e37b2447f50d2cb6f95e20169192620f42
Karl Stevens <karl.e.stevens@gmail.com>
2020-12-09 12:44:38 -0600

    Issue #4

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { plantSaleYear } from '../shared/config';
 
 function Hero(props) {
   return (
     <div className="hero">
       <div className="flex-center">
-          <p className="text-5xl font-semibold">[Placeholder  Year] Plant Sale</p>
+          <p className="text-5xl font-semibold">{plantSaleYear} Plant Sale</p>
       </div>
     </div>
   )
