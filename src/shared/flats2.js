eb5dc5bd9ef6001ae4f0ede6cc5b2a7c6a27f9f5
betsystevens <betsystevens5@gmail.com>
2020-12-10 11:46:08 -0600

    add version 2 of flats data file

---
+++ src/shared/flats2.js
@@ -0,0 +1,142 @@
+export const FLATS2 =
+[
+  {
+    id: 0,
+    name: 'Alyssum',
+    variety: [
+      { name: 'mixed', image: '/assets/images/flats/alyssumMixed512x138.jpg'},
+      { name: 'white', image: '/assets/images/flats/alyssumWhite512x384.jpg'},
+      { name: 'purple', image: '/assets/images/flats/allysumPurple512x384.jpg'} 
+    ],
+    container: 'flat',
+    description: 'Lorem ipsum dolor dolor dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+  },
+  {
+    id: 1,
+    name: 'Begonia',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+    variety: [
+      { name: 'mixed', image: '/assets/images/flats/begoniaMixed512x384.jpg'},
+      { name: 'white', image: '/assets/images/flats/begoniaWhite512x384.jpg'},
+      { name: 'pink', image: '/assets/images/flats/begoniaPink512x384.jpg'},
+      { name: 'red', image: '/assets/images/flats/begoniaRed512x384.jpg'},
+      { name: 'pink bronze leaf', image: '/assets/images/flats/begoniaPinkBronzeLeaf512x384.jpg'},
+      { name: 'red bronze leaf', image: '/assets/images/flats/begoniaRedBronzeLeaf512x384.jpg'}
+    ]
+  },
+  {
+    id: 2,
+    name: 'Coleus',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+    variety: [
+      { name: 'mixed', image: '/assets/images/flats/coleusWizardMix512x384.jpg'},
+      { name: 'scarlet', image: '/assets/images/flats/coleusScarlet512x384.jpg'}
+    ] 
+  },
+  {
+    id: 3,
+    name: 'Dahlia',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+    variety: [
+      { name: 'mixed', image: '/assets/images/flats/dahliaMixed512x384.jpg' }
+    ] 
+  },
+  {
+    id: 4,
+    name: 'Dianthus',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+    variety: [
+      { name: 'mixed', image: '/assets/images/flats/dianthusMixed512x384.jpg'},
+      { name: 'red', image: '/assets/images/flats/dianthusRed512x384.jpg'}
+    ]
+  },
+  {
+    id: 5,
+    name: 'Dusty Miller',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+    variety: [ { name: 'silver', image: '/assets/images/flats/dustyMiller512x384.jpg' } ]
+  },
+  {
+    id: 6,
+    name: 'Impatiens',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+    variety: [
+      { name: 'mixed', image: '/assets/images/flats/impatiensPastelMix512x384.jpg'},
+      { name: 'white', image: '/assets/images/flats/impatiensWhite512x384.jpg'},
+       { name: 'red', image: '/assets/images/flats/impatiensRed512x384.jpg'},
+       { name: 'salmom', image: '/assets/images/flats/impatiensSalmon512x384.jpg'},
+       { name: 'violet', image: '/assets/images/flats/impatiensViolet512x384.jpg'},
+       { name: 'hot pink', image: '/assets/images/flats/impatiensPink512x384.jpg'},
+      ]
+  },
+  {
+    id: 7,
+    name: 'Marigold',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+    variety: [
+      { name: 'Bi-color', image: '/assets/images/flats/marigoldMixed512x384.jpg'},
+      { name: 'Yellow', image: '/assets/images/flats/marigoldYellow512x384.jpg'},
+      { name: 'Orange', image: '/assets/images/flats/marigoldOrange512x384.jpg'},
+    ]
+  },
+  {
+    id: 8,
+    name: 'Petunia',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+    variety: [
+      { name: 'Mixed', image: '/assets/images/flats/petuniaMixed512x384.jpg'},
+      { name: 'Midnight Blue', image: '/assets/images/flats/petuniaMidnight512x384.jpg'},
+      { name: 'Pink', image: '/assets/images/flats/petuniaPink512x384.jpg'},
+      { name: 'Red', image: '/assets/images/flats/petuniaRed512x384.jpg'},
+      { name: 'White', image: '/assets/images/flats/petuniaWhite512x384.jpg'},
+      { name: 'Yellow', image: '/assets/images/flats/petuniaYellow512x384.jpg'}
+    ]
+  },
+  {
+    id: 9,
+    name: 'Salvia',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+    variety: [
+      { name: 'Blue', image: '/assets/images/flats/salviaVictoriaBlue512x384.jpg'},
+      { name: 'Red', image: '/assets/images/flats/salviaRed512x384.jpg'}
+    ]
+  },
+  {
+    id: 10,
+    name: 'Snap Dragons',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+    variety: [
+      { name: 'dwarf mixed', image: '/assets/images/flats/snapDragonMix512x384.jpg'},
+      { name: 'tall mixed', image: '/assets/images/flats/snapDragonsTall512x384.jpg'}
+    ]
+  },
+  {
+    id: 11,
+    name: 'Vinca',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+    variety: [
+      { name: 'mixed', image: '/assets/images/flats/vincaMixed512x384.jpg'},
+      { name: 'red', image: '/assets/images/flats/vincaRed512x384.jpg'},
+      { name: 'white', image: '/assets/images/flats/vincaWhite512x384.jpg'},
+      { name: 'lavender', image: '/assets/images/flats/vincaLavender512x384.jpg'}
+    ]
+  },
+  {
+    id: 12,
+    name: 'Zinnia',
+    container: 'flat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+    variety: [ { name: 'mixed', image: '/assets/images/flats/zinniaMixed512x384.jpg' } ]
+  }
+];
