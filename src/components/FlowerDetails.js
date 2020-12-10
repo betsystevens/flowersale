1375a4b39e9db641755ea2a0d7260fe0aa892396
betsystevens <betsystevens5@gmail.com>
2020-12-10 00:51:34 -0600

    change big image when hover on thumbnails, refactor

--- src/components/FlowerDetails.js
+++ src/components/FlowerDetails.js
@@ -1,48 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function FlowerDetails({ flower, flowerGroup }) {
 
-  const thumbnails = flower.image.map((image, index) => {
+function BigImage(props) {
+  return (
+    <img className="w-96" src={props.flower.image} alt={props.flower.name} />
+  )
+}
+class FlowerDetails extends React.Component  {
+  state = {
+    imageId: 0,
+    variety: {}
+  }
+  handleMouseEnter = (index) => {
+    console.log(index);
+    this.setState({ imageId: index })
+  }
+
+  thumbnails = this.props.flower.variety.map((variety, index) => {
     return (
-      <div key={index} className="">
-        <img src={image} alt={"flower"} className="w-20 p-1"/>
-      </div>
+    <div key={index} onMouseEnter={() => this.handleMouseEnter(index)} >
+      <img src={variety.image} alt={variety.name} className="w-20 p-1"/>
+    </div>
     )
   })
-  const image = flower.image[0]
-  const description = flowerGroup[0].container.description
-  const price = flowerGroup[0].price
-  let variety = flower.variety[0]
-  return (
-    // <div className="mt-9 flex justify-center">
-    <div className="mt-16 ml-16">
+
+  render() {
+    const { name, variety } = this.props.flower
+    const { container, price } = this.props.flowerGroup
+    return (
+      <div className="mt-16 ml-16">
       <div className="gridDetailWrapper">
         <div>
-          <p className="font-extrabold text-lg">{flower.name}</p>
-          <p className="pt-2">{`Flat - ${description}` }</p>
+          <p className="font-extrabold text-lg">{name}</p>
+          <p className="pt-2">{`Flat - ${container.description}` }</p>
         </div>
         <p className="pt-10">{`Price: $${price}` }</p>
-          <div className="spanRows">
-            <div className="flex flex-col">
-              <Link to={`/`} >
-                <p className="pt-36 underline">Checkout</p>
-              </Link>
-              <Link to={`/flats`} >
-                 <p className="pt-16 underline">Continue Shopping</p>
-              </Link>
-            </div>
+        <div className="spanRows">
+          <div className="flex flex-col">
+            <Link to={`/`} className="mt-36">
+              <p className="underline hover:text-purple-500">Checkout</p>
+            </Link>
+            <Link to={`/flats`} className="mt-16">
+                <p className="underline hover:text-purple-500">Continue Shopping</p>
+            </Link>
           </div>
-          <img className="w-96" src={image} alt={flower.name} />
-          <div>
-            <p className="topRow">{`Variety: ${variety}` }</p>
-            <div className="pt-4 h-65 grid grid-rows-2 grid-cols-3 gap-2">
-              {thumbnails}
-            </div>
+        </div>
+        <BigImage flower={{image: variety[this.state.imageId].image, name: this.props.flower.name}} />
+        <div>
+          <p className="topRow">{`Variety: ${variety[0].name}` }</p>
+          <div className="pt-4 h-65 grid grid-rows-2 grid-cols-3 gap-2">
+            {this.thumbnails}
           </div>
+        </div>
       </div>
     </div>
-  )
+  )}
 }
 
 export default FlowerDetails;
