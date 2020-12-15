82ca9c91f0661c054bfc3726ad231876d03cd79d
betsystevens <betsystevens5@gmail.com>
2020-12-14 21:56:33 -0600

    bug fix, enlarged clickable area of plus/minus symbols on quantity button

--- src/components/FlowerDetails.js
+++ src/components/FlowerDetails.js
@@ -18,17 +18,23 @@ function AddToCart(props) {
 function Quantity(props) {
   return (
     <div className="mt-6">
-      <div className="cursor-pointer border-2 border-gray-200 bg-gray-100 rounded relative h-7 w-20">
-        <span onClick={props.handleMinus} className="minus "></span>
-        <input
-          className="absolute left-6 w-7 h-full bg-gray-50 text-center font-mono font-thin text-sm"
-          type="number"
-          min="0"
-          max="99"
-          value={props.quantity}
-          onChange={props.handleOnChange}
-        ></input>
-        <span onClick={props.handlePlus} className="plus"></span>
+      <div className="flex cursor-pointer border-2 border-gray-200 bg-gray-100 rounded relative h-7 w-20">
+        <div onClick={props.handleMinus} className="relative h-full w-1/3">
+          <div className="minus absolute"></div>
+        </div>
+        <div className="relative h-full w-1/3">
+          <input
+            className="w-full h-full bg-gray-50 text-center font-mono font-thin text-sm"
+            type="number"
+            min="0"
+            max="99"
+            value={props.quantity}
+            onChange={props.handleOnChange}
+          ></input>
+        </div>
+        <div onClick={props.handlePlus} className="relative h-full w-1/3">
+          <div className="plus absolute"></div>
+        </div>
       </div>
     </div>
   );
