65157bec59586b66b084ce3d266ddedc9e7d7cfb
betsystevens <betsystevens5@gmail.com>
2020-12-16 22:51:35 -0600

    automatic prettier style changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,48 +1,57 @@
-import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import Header from  './Header';
-import Hero from  './Hero';
-import FlowerGroups from './FlowerGroups';
-import FlowerCards from './FlowerCards';
-import Radios from './Radios';
-import FlowerDetails from './FlowerDetails';
-import { FLOWERS } from '../shared/flowers';
-import { FLATS } from '../shared/flats';
-import { FLATS2 } from '../shared/flats2';
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Header from "./Header";
+import Hero from "./Hero";
+import FlowerGroups from "./FlowerGroups";
+import FlowerCards from "./FlowerCards";
+import Radios from "./Radios";
+import FlowerDetails from "./FlowerDetails";
+import { FLOWERS } from "../shared/flowers";
+import { FLATS } from "../shared/flats";
+import { FLATS2 } from "../shared/flats2";
 
-import '../App.css';
+import "../App.css";
 
 function Main(props) {
-
   const data = {
-    flowers : FLOWERS,
-    flats : FLATS,
-    flats2 : FLATS2
-  }
+    flowers: FLOWERS,
+    flats: FLATS,
+    flats2: FLATS2,
+  };
 
   const HomePage = () => {
-    return(
+    return (
       <div>
         <Hero />
         <FlowerGroups flowers={data.flowers} />
       </div>
-    )
-  }
+    );
+  };
 
   return (
-
     <div>
       <React.StrictMode>
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
-          <Route exact path="/flats" component={() => <FlowerCards flats={data.flats} flowers={data.flowers} />} />
-          <Route 
-            exact path="/flats/:flowerId" 
-            render={ ({match}) => (
-              <FlowerDetails 
+          <Route
+            exact
+            path="/flats"
+            component={() => (
+              <FlowerCards flats={data.flats} flowers={data.flowers} />
+            )}
+          />
+          <Route
+            exact
+            path="/flats/:flowerId"
+            render={({ match }) => (
+              <FlowerDetails
                 flower={data.flats2[match.params.flowerId]}
-                flowerGroup={data.flowers.filter(flower => flower.container.name === 'flat')[0]}
+                flowerGroup={
+                  data.flowers.filter(
+                    (flower) => flower.container.name === "flat"
+                  )[0]
+                }
               />
             )}
           />
@@ -51,7 +60,7 @@ function Main(props) {
         </Switch>
       </React.StrictMode>
     </div>
-  )
+  );
 }
 
 export default Main;
