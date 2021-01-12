09987eeed64ffcaddaf06c6cef2a83a3f2ee40d8
betsystevens <betsystevens5@gmail.com>
2021-01-11 22:01:06 -0600

    change image alt to variable from literal

--- src/components/BigImage.js
+++ src/components/BigImage.js
@@ -1,12 +1,8 @@
 import React from "react";
 
-function BigImage(props) {
+function BigImage({ image, name }) {
   return (
-    <img
-      src={props.flower.image}
-      alt={props.flower.name}
-      className="p-1.5 shadow-md rounded-md w-96"
-    />
+    <img src={image} alt={name} className="p-1.5 shadow-md rounded-md w-96" />
   );
 }
 export default BigImage;
