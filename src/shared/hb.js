f75344574bffe4b03f880ca5210eb9679970b898
betsystevens <betsystevens5@gmail.com>
2021-01-21 18:17:19 -0600

    add hb data file

---
+++ src/shared/hb.js
@@ -0,0 +1,155 @@
+export const HB = [
+  {
+    id: 0,
+    name: "Combo Basket",
+    container: "hb",
+    description: "Lorem ipsum",
+    variety: [
+      {
+        name: "blue purple",
+        image: "/assets/images/hb/comboBluePurple512x384.jpg",
+      },
+      { name: "pink", image: "/assets/images/hb/comboPinkComp512x384.jpg" },
+      {
+        name: "red whit blue",
+        image: "/assets/images/hb/comboRedWhiteBlue512x384.jpg",
+      },
+      {
+        name: "yellow blue",
+        image: "/assets/images/hb/comboRedYellowBlue512x384.jpg",
+      },
+    ],
+  },
+  {
+    id: 1,
+    // name: "Double Rosebud Impatiens",
+    name: "Dbl Rosebud Impatiens",
+    container: "hb",
+    description: "Lorem ipsum",
+    variety: [
+      { name: "pink", image: "/assets/images/hb/dblRosebudPinkHB512x384.jpg" },
+      {
+        name: "sparkler hot pink",
+        image: "/assets/images/hb/dblRosebudSparklerHotPinkHB512x384.jpg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Dragon Wing Begonia",
+    variety: [
+      {
+        name: "red",
+        image: "/assets/images/hb/dragonWingBegoniaRed512x384.jpg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Fuschia",
+    container: "hb",
+    description: "Lorem ipsum",
+    variety: [
+      {
+        name: "red purple",
+        image: "/assets/images/hb/fuschiaRedPurple512x384.jpg",
+      },
+      {
+        name: "red white",
+        image: "/assets/images/hb/fuschiaRedWhite512x384.jpg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Illumination Begonia",
+    container: "hb",
+    description: "Lorem ipsum",
+    variety: [
+      {
+        name: "apricot",
+        image: "/assets/images/hb/illuminationBegoniaApricot512x384.jpg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Impatiens",
+    container: "hb",
+    description: "Lorem ipsum",
+    variety: [
+      { name: "mixed", image: "/assets/images/hb/impatiensMixedHB512x384.jpg" },
+      { name: "pink", image: "/assets/images/hb/impatiensPinkHB512x384.jpg" },
+      { name: "red", image: "/assets/images/hb/impatiensRed512x384.jpg" },
+    ],
+  },
+  {
+    id: 6,
+    name: "Ivy Gernanium",
+    container: "hb",
+    description: "Lorem ipsum",
+    variety: [
+      {
+        name: "lavender",
+        image: "/assets/images/hb/ivyGeraniumLavenderHB512x384.jpg",
+      },
+      { name: "pink", image: "/assets/images/hb/ivyGeraniumPinkHB512x384.jpg" },
+      { name: "red", image: "/assets/images/hb/ivyGeraniumRedHB512x384.jpg" },
+      {
+        name: "salmon",
+        image: "/assets/images/hb/ivyGeraniumSalmonHB512x384.jpg",
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: "Million Bells",
+    container: "hb",
+    description: "Lorem ipsum",
+    variety: [
+      {
+        name: "blue",
+        image: "/assets/images/hb/millionBellsBlueHB512x384.jpg",
+      },
+      {
+        name: "pink",
+        image: "/assets/images/hb/millionBellsPinkHB512x384.jpg",
+      },
+      { name: "red", image: "/assets/images/hb/millionBellsRedHB512x384.jpg" },
+      {
+        name: "yellow",
+        image: "/assets/images/hb/millionBellsYellowHB512x384.jpg",
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: "Petunia",
+    container: "hb",
+    description: "Lorem ipsum",
+    variety: [
+      {
+        name: "bordeaux",
+        image: "/assets/images/hb/petuniaBordeauxHB512x384.jpg",
+      },
+      {
+        name: "burgundy star",
+        image: "/assets/images/hb/petuniaBurgundyStarHB512x384.jpg",
+      },
+      {
+        name: "midnight blue",
+        image: "/assets/images/hb/petuniaMidnightBlueHB512x384.jpg",
+      },
+      { name: "pink", image: "/assets/images/hb/petuniaPinkHB512x384.jpg" },
+      {
+        name: "pink purple",
+        image: "/assets/images/hb/petuniaPinkPurpleHB512x384.jpg",
+      },
+      { name: "purple", image: "/assets/images/hb/petuniaPurpleHB512x384.jpg" },
+      {
+        name: "sky blue",
+        image: "/assets/images/hb/petuniaSkyBlueHB512x384.jpg",
+      },
+    ],
+  },
+];
--- src/utils.bash
+++
@@ -1,2 +0,0 @@
-// copy files into a directory, file names are read from file: imgFileNames.txt; 
-while read file; do cp "$file" tmpDir; done < imgFileNames.txt;
