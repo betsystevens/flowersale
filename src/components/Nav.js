e60f29c49ff1d6c934252b3c5639c1c552cf59a0
betsystevens <betsystevens5@gmail.com>
2021-02-04 11:33:31 -0600

    add responsive column nav for mobile

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,47 +6,117 @@ function Nav(props) {
   return (
     <div className="sticky top-0">
       <nav className="flex justify-between items-center">
-        <ul>
-          <li className="hidden sm:block">
-            <NavLink to="/" exact>
-              <svg className="pt-1 w-12 h-12 fill-current text-indigo-100">
-                <use xlinkHref="/assets/icons/noun_flowers_ext.svg#icon-flowers" />
-              </svg>
-            </NavLink>
-          </li>
-          <li className="block sm:hidden">
-            <button className="block mx-auto">
-              <svg className="w-6 h-6 mx-auto fill-current text-indigo-100">
-                <use xlinkHref="/assets/icons/menu.svg#icon-menu" />
-              </svg>
-            </button>
-          </li>
-        </ul>
-        <ul className="flex">
-          <li className="hidden sm:block text-sm md:text-lg">
-            <NavLink to="/flat" exact className="hover:text-gray-50">
-              Flats
-            </NavLink>
-          </li>
-          <li className="hidden sm:block text-sm md:text-lg">
-            <NavLink to="/hb" exact className="hover:text-gray-50">
-              Hanging Baskets
-            </NavLink>
-          </li>
-          <li className="hidden sm:block text-sm md:text-lg">
-            <NavLink to="/pot" exact className="hover:text-gray-50">
-              Potted Plants
-            </NavLink>
-          </li>
-          <li className="hidden sm:block text-sm md:text-lg">
-            <NavLink to="/herbTomato" exact className="hover:text-gray-50">
-              Herbs & Tomatoes
-            </NavLink>
-          </li>
+        {/* nav row */}
+        <div className="hidden sm:block">
+          {/* <ul className="flex items-center text-base md:text-lg"> */}
+          <ul className="flex items-center text-lg">
+            <li className="">
+              <NavLink to="/" exact>
+                <svg className="py-1 w-12 h-12 fill-current text-indigo-100">
+                  <use xlinkHref="/assets/icons/noun_flowers_short.svg#icon-flowers" />
+                </svg>
+              </NavLink>
+            </li>
+            <div className="flex flex-wrap">
+              <li>
+                <NavLink
+                  to="/flat"
+                  exact
+                  className="hover:text-gray-50 whitespace-nowrap"
+                >
+                  Flats
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/hb"
+                  exact
+                  className="hover:text-gray-50 whitespace-nowrap"
+                >
+                  Hanging Baskets
+                </NavLink>
+              </li>
+            </div>
+            <div className="flex flex-wrap">
+              <li>
+                <NavLink
+                  to="/pot"
+                  exact
+                  className="hover:text-gray-50 whitespace-nowrap"
+                >
+                  Potted Plants
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/herbTomato"
+                  exact
+                  className="hover:text-gray-50 whitespace-nowrap"
+                >
+                  Herbs & Tomatoes
+                </NavLink>
+              </li>
+            </div>
+          </ul>
+        </div>
+        {/* end of nav row */}
+
+        {/* nav column */}
+        <div className="block sm:hidden border-2 border-red-300">
+          <ul className="flex flex-col text-base">
+            <li className="">
+              <button className="block py-0.5 px-1 rounded ring-2 ring-opacity-10 ring-gray-50 ">
+                <svg className="w-4 h-4 mx-auto fill-current text-indigo-100">
+                  <use xlinkHref="/assets/icons/menu.svg#icon-menu" />
+                </svg>
+              </button>
+            </li>
+            <li>
+              <NavLink
+                to="/flat"
+                exact
+                className="hover:text-gray-50 whitespace-nowrap"
+              >
+                Flats
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/hb"
+                exact
+                className="hover:text-gray-50 whitespace-nowrap"
+              >
+                Hanging Baskets
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/pot"
+                exact
+                className="hover:text-gray-50 whitespace-nowrap"
+              >
+                Potted Plants
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/herbTomato"
+                exact
+                className="hover:text-gray-50 whitespace-nowrap"
+              >
+                Herbs & Tomatoes
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        {/* end of nav column */}
+
+        {/* cart icon link */}
+        <ul className="flex justify-around">
           <li className="flex-shrink-0 self-center">
             <NavLink to="/cart" exact>
               <img
-                className="h-5 md:h-8 cursor-pointer"
+                className="h-8 cursor-pointer"
                 src="/assets/icons/CartWhite.svg"
                 alt="cart"
               />
