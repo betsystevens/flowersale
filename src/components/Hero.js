d2861a0c8c761a61ee6179e4510aaf15e0c6c411
betsystevens <betsystevens5@gmail.com>
2021-02-04 16:04:49 -0600

    add mobile responsiveness to Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { plantSaleYear } from "../shared/config";
 
-function Hero(props) {
+function Hero() {
   return (
     <div className="hero">
-      <div className="flex flex-col items-center">
-        <p className="mb-4 text-5xl font-semibold">Scout Troop 34</p>
-        <p className="mb-5 text-5xl font-semibold">Plant Sale</p>
-        <p className="text-2xl font-semibold">{plantSaleYear}</p>
+      <div className="h-32 md:h-64 flex flex-col justify-evenly items-center font-semibold">
+        <p className="text-2xl md:text-5xl">Scout Troop 34</p>
+        <p className="text-2xl md:text-5xl">Plant Sale</p>
+        <p className="hidden md:block text-2xl">{plantSaleYear}</p>
       </div>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,32 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
+function NavItem({ location, label }) {
+  return (
+    <li>
+      <NavLink
+        to={location}
+        exact
+        className="hover:text-gray-50 whitespace-nowrap"
+      >
+        {label}
+      </NavLink>
+    </li>
+  );
+}
 function Nav(props) {
   console.log(window.innerWidth);
+  const handleClick = () => {
+    console.log("menu icon clicked");
+    let value = display === "hidden" ? "block" : "hidden";
+    setDisplay(value);
+  };
+  const [display, setDisplay] = useState("hidden");
   return (
     <div className="sticky top-0">
       <nav className="flex justify-between items-center">
-        {/* nav row */}
+        {/* wide nav - row */}
         <div className="hidden sm:block">
-          {/* <ul className="flex items-center text-base md:text-lg"> */}
           <ul className="flex items-center text-lg">
             <li className="">
               <NavLink to="/" exact>
@@ -18,111 +36,53 @@ function Nav(props) {
               </NavLink>
             </li>
             <div className="flex flex-wrap">
-              <li>
-                <NavLink
-                  to="/flat"
-                  exact
-                  className="hover:text-gray-50 whitespace-nowrap"
-                >
-                  Flats
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/hb"
-                  exact
-                  className="hover:text-gray-50 whitespace-nowrap"
-                >
-                  Hanging Baskets
-                </NavLink>
-              </li>
+              <NavItem location="/flat" label="Flats" />
+              <NavItem location="/hb" label="Hanging Baskets" />
             </div>
             <div className="flex flex-wrap">
-              <li>
-                <NavLink
-                  to="/pot"
-                  exact
-                  className="hover:text-gray-50 whitespace-nowrap"
-                >
-                  Potted Plants
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/herbTomato"
-                  exact
-                  className="hover:text-gray-50 whitespace-nowrap"
-                >
-                  Herbs & Tomatoes
-                </NavLink>
-              </li>
+              <NavItem location="/pot" label="Potted Plants" />
+              <NavItem location="/herbTomato" label="Herbs & Tomatoes" />
             </div>
           </ul>
         </div>
-        {/* end of nav row */}
+        {/* end of wide nav - row */}
 
-        {/* nav column */}
-        <div className="block sm:hidden border-2 border-red-300">
+        {/* mobile nav - column */}
+        <div className="block sm:hidden">
           <ul className="flex flex-col text-base">
-            <li className="">
-              <button className="block py-0.5 px-1 rounded ring-2 ring-opacity-10 ring-gray-50 ">
+            <li className="block">
+              <button
+                onClick={handleClick}
+                className="block py-0.5 px-1 rounded ring-2 ring-opacity-10 ring-gray-50 "
+              >
                 <svg className="w-4 h-4 mx-auto fill-current text-indigo-100">
                   <use xlinkHref="/assets/icons/menu.svg#icon-menu" />
                 </svg>
               </button>
             </li>
-            <li>
-              <NavLink
-                to="/flat"
-                exact
-                className="hover:text-gray-50 whitespace-nowrap"
-              >
-                Flats
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/hb"
-                exact
-                className="hover:text-gray-50 whitespace-nowrap"
-              >
-                Hanging Baskets
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/pot"
-                exact
-                className="hover:text-gray-50 whitespace-nowrap"
-              >
-                Potted Plants
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/herbTomato"
-                exact
-                className="hover:text-gray-50 whitespace-nowrap"
-              >
-                Herbs & Tomatoes
-              </NavLink>
-            </li>
+
+            <div className={display}>
+              <NavItem location="/" label="Home" />
+              <NavItem location="/flat" label="Flats" />
+              <NavItem location="/hb" label="Hanging Baskets" />
+              <NavItem location="/pot" label="Potted Plants" />
+              <NavItem location="/herbTomato" label="Herbs & Tomatoes" />
+              <NavItem location="/cart" label="Cart" />
+            </div>
           </ul>
         </div>
-        {/* end of nav column */}
+        {/* end of wide nav - column */}
 
         {/* cart icon link */}
-        <ul className="flex justify-around">
-          <li className="flex-shrink-0 self-center">
-            <NavLink to="/cart" exact>
-              <img
-                className="h-8 cursor-pointer"
-                src="/assets/icons/CartWhite.svg"
-                alt="cart"
-              />
-            </NavLink>
-          </li>
-        </ul>
+        <div className="hidden sm:block mr-2">
+          <NavLink to="/cart" exact>
+            <img
+              className="h-8 cursor-pointer"
+              src="/assets/icons/CartWhite.svg"
+              alt="cart"
+            />
+          </NavLink>
+        </div>
       </nav>
     </div>
   );
