496f7508843d104d9cb482828b1d4da6b1bd6d22
betsystevens <betsystevens5@gmail.com>
2021-02-04 22:47:02 -0600

    add short description, advance mobile design

--- src/components/FlowerGroups.js
+++ src/components/FlowerGroups.js
@@ -7,13 +7,24 @@ function FlowerGroups() {
   const group = GROUPS.map((flower) => {
     return (
       <Link to={`/${flower.group}`} key={flower.id}>
-        <div className="flex flex-nowrap p-5 mx-20 my-12 shadow">
-          <div className="flex-shrink-0">
-            <img src={flower.image} alt={flower.name} className="w-56 h-36" />
+        <div className="flex flex-col sm:flex-row p-5 mx-4 sm:mx-20 my-12 shadow">
+          <div className="flex-shrink-0 self-center sm:self-start">
+            <img
+              src={flower.image}
+              alt={flower.name}
+              className="w-64 h-48 sm:w-56 sm:h-36 mb-2"
+            />
           </div>
-          <div className="flex-col pl-5">
-            <p className="text-xl font-semibold">{flower.name}</p>
-            <p className="pt-4">{flower.description}</p>
+          <div className="flex flex-col pl-5">
+            <p className="self-center sm:self-start text-xl font-semibold">
+              {flower.name}
+            </p>
+            <p className="pt-4 hidden md:block md:self-start">
+              {flower.description}
+            </p>
+            <p className="pt-4 block md:hidden self-center md:self-start">
+              {flower.shortDescription}
+            </p>
           </div>
         </div>
       </Link>
--- src/shared/groups.js
+++ src/shared/groups.js
@@ -6,6 +6,8 @@ export const GROUPS = [
     image: "assets/images/flats/dahliaMixed512x384.jpg",
     description:
       "A variety of annuals that are guaranteed to start your garden blooming. Choose from an assortment of colors and sizes to create your own personal floral masterpiece. Thirty six plants per flat that were given the best of care and are ready to bloom for you all summer.",
+    shortDescription:
+      "A variety of annuals that are guaranteed to start your garden blooming. There are 36 plants per flat",
   },
   {
     id: 1,
@@ -14,6 +16,8 @@ export const GROUPS = [
     image: "assets/images/hb/petuniaPinkPurpleHB750x500.jpg",
     description:
       "Our 10-inch flower baskets are of unbeatable quality. They regularly sell out every year. Never disappointing, these spectacular baskets keep customers coming back year after year. The wave petunias and ivy geraniums are unrivaled in their beauty and quality.",
+    shortDescription:
+      "Our 10-inch flower baskets are of unbeatable quality. They regularly sell out every year.",
   },
   {
     id: 2,
@@ -22,6 +26,8 @@ export const GROUPS = [
     image: "assets/images/pots/margueriteDaisy512x384.jpg",
     description:
       "Annuals available in 4-inch, 6-inch and gallon sized pots. As always, we offer the eternally popular zonal geraniums. The purple annual fountain grass is always a hit and by fall it will be the star of your garden. Accent plants and ready to go patio pots are the best and so easy.",
+    shortDescription:
+      "Annuals and accent plants available in 4-inch, 6-inch and gallon sized pots.",
   },
   {
     id: 3,
@@ -30,5 +36,7 @@ export const GROUPS = [
     image: "assets/images/pots/basilHerb720x480.jpg",
     description:
       "Choose from our selection of popular herbs: basil, oregano, parsley, rosemary, chives, and cilantro. For the crown jewel of the home vegetable garden, delicious homegrown tomatoes, we offer several reliable varieties in 4-inch pots.",
+    shortDescription:
+      "Choose from a selection of popular herbs and tomatoes avaialable in 4-inch pots.",
   },
 ];
