9be2c2a9942e613382dce35a28684cca7ff6cc21
betsystevens <betsystevens5@gmail.com>
2021-02-07 05:41:20 -0600

    add mobile responsiveness to cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -149,32 +149,65 @@ function CartSubtotal({ cart }) {
   }
   return (
     <div
-      className="order-first mdlg:order-last mdlg:w-1/4 h-36 mdlg:h-52 
-                mb-2 mdlg:mb-0 mdlg:mt-0.5 pb-2 flex-grow
-                  flex mdlg:flex-col justify-around items-center
-                  bg-white shadow-lg"
+      className="order-first mdlg:order-last
+                  w-3/4 mdlg:w-1/4
+                  mb-2 mdlg:mb-0 mdlg:mt-0.5 pb-2"
     >
-      <img
-        className="mdlg:pt-6 px-4 h-12 mdlg:h-16"
-        src="/assets/icons/Cart64x40.svg"
-        alt="cart"
-      />
-      <p className="text-base md:text-lg lg:text-xl text-center">
-        Subtotal: <span className="font-semibold">${subTotal}</span>
-      </p>
-      <p className="text-lg">
-        {qtySum} {qtySum === 1 ? " item" : " items"}{" "}
-      </p>
+      <div
+        className="hidden mdlg:flex flex-col justify-evenly items-center
+                    bg-white shadow-lg
+                    mdlg:h-52"
+      >
+        <img
+          className="sm:pt-4 h-12 md:h-16"
+          src="/assets/icons/Cart64x40.svg"
+          alt="cart"
+        />
+        <p className="text-base md:text-lg lg:text-xl">
+          Subtotal: <span className="font-semibold">${subTotal}</span>
+        </p>
+        <p className="text-lg">
+          {qtySum} {qtySum === 1 ? " item" : " items"}{" "}
+        </p>
+        <Link to={`/printOrder`}>
+          <button
+            className="border-2 border-gray-200 bg-gray-100 rounded
+                h-8 px-2 text-sm
+                hover:text-purple-500"
+          >
+            Printer Friendly Order
+          </button>
+        </Link>
+      </div>
 
-      <Link to={`/printOrder`}>
-        <button
-          className="border-2 border-gray-200 bg-gray-100 rounded 
-              h-8 px-2 mx-auto text-sm
-              hover:text-purple-500"
-        >
-          Printer Friendly Order
-        </button>
-      </Link>
+      {/* mobile stacked */}
+      <div
+        className="bg-white shadow-lg h-80 w-60 mx-auto
+                   flex flex-col justify-evenly items-center
+                   sm:flex-row sm:h-32 sm:w-96 flex-wrap justify-evenly items-center
+                   mdlg:hidden"
+      >
+        <img
+          className="sm:pt-4 h-12 sm:h-16"
+          src="/assets/icons/Cart64x40.svg"
+          alt="cart"
+        />
+        <p className="text-lg">
+          Subtotal: <span className="font-semibold">${subTotal}</span>
+        </p>
+        <p className="text-lg">
+          {qtySum} {qtySum === 1 ? " item" : " items"}{" "}
+        </p>
+        <Link to={`/printOrder`}>
+          <button
+            className="border-2 border-gray-200 bg-gray-100 rounded
+                h-8 px-2 text-sm
+                hover:text-purple-500"
+          >
+            Printer Friendly Order
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
