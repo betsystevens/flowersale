85dd049ac6d2e4ff39b2deb6479905ad55ed7b3f
betsystevens <betsystevens5@gmail.com>
2021-02-17 23:05:39 -0600

    refactor, use consistent arrow function style

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -1,28 +1,28 @@
 import { PRICING } from "../shared/pricing";
 
-const getPrice = (container) => {
+export const getPrice = (container) => {
   return PRICING.filter(
     (flowerGroup) => flowerGroup.container.name === container
   )[0].container.price;
 };
 
-const getContainerDescription = (container) => {
+export const getContainerDescription = (container) => {
   return PRICING.filter(
     (flowerGroup) => flowerGroup.container.name === container
   )[0].container.description;
 };
-function getItemsCount(cart) {
+
+export const getItemsCount = (cart) => {
   let count = cart.reduce((accumulator, item) => {
     return accumulator + item.quantity;
   }, 0);
   return count;
-}
-function computeSubtotal(cart) {
+};
+export const computeSubtotal = (cart) => {
   let subTotal = 0;
   cart.forEach((flower) => {
     let price = getPrice(flower.container);
     subTotal = subTotal + flower.quantity * price;
   });
   return subTotal;
-}
-export { getPrice, getContainerDescription, computeSubtotal, getItemsCount };
+};
