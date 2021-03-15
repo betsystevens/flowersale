4b55493499313ca2ff1448d3f1566b710a200ad3
betsystevens <betsystevens5@gmail.com>
2021-03-14 21:19:41 -0500

    add .env file instead of using config file

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,14 @@
-import React from "react";
-import { plantSaleYear } from "../shared/config";
+import React from 'react';
 
 function Hero() {
   return (
-    <div className="hero">
-      <div className="h-32 md:h-64 flex flex-col justify-evenly items-center font-semibold">
-        <p className="text-2xl md:text-5xl">Scout Troop 34</p>
-        <p className="text-2xl md:text-5xl">Plant Sale</p>
-        <p className="hidden md:block text-2xl">{plantSaleYear}</p>
+    <div className='hero'>
+      <div className='h-32 md:h-64 flex flex-col justify-evenly items-center font-semibold'>
+        <p className='text-2xl md:text-5xl'>
+          {process.env.REACT_APP_FUNDRAISER_NAME}
+        </p>
+        <p className='text-2xl md:text-5xl'>Plant Sale</p>
+        <p className='hidden md:block text-2xl'>{process.env.REACT_APP_YEAR}</p>
       </div>
     </div>
   );
--- src/shared/config.js
+++
@@ -1,1 +0,0 @@
-export const plantSaleYear = '2021';
